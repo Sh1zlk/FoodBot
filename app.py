3308cf942fa002b55360c64d31e45dca28756562
Sergei1213 <shmalovsergei@gmail.com>
2023-07-19 12:36:51 +0300

    Add main files

---
+++ app.py
@@ -0,0 +1,20 @@
+# -----------------------------------------
+# Script by Andriy.K.
+#
+#
+# Version       Date       Info
+# 1.0           2023       Initial Version
+#
+# -----------------------------------------
+
+# import requests
+# from urllib.parse import quote
+# # r = requests.get('https://shuba.life/search?term=%D0%BA%D0%B0%D0%B1%D0%B0%D1%87%D0%BE%D0%BA')
+# # print(r.text)
+#
+# text = 'Кабачок'
+# encod_world = quote(text, encoding='utf-8')
+# print(encod_world)
+# r = requests.get(f'https://shuba.life/search?term={encod_world}')
+# with open('index.html', 'w') as f:
+#     f.write(r.text)
