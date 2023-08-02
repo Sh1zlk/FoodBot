c094e1b13bc2000e77931e2c3972fda709f53f7a
Sergeri1213 <shmalovsergei@gmail.com>
2023-08-02 21:55:49 +0300

    Finish bot project v2.0

--- database/models/database.py
+++ database/models/database.py
@@ -6,16 +6,15 @@
 # 1.0           2023       Initial Version
 #
 # -----------------------------------------
-
+import os
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# DATABASE_NAME = 'food.db'
-DATABASE_NAME ='food.sqlite'
+DATABABE_USER = os.environ.get('DATABABE_USER')
+DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD')
 
-# engine = create_engine(f'sqlite:///database/{DATABASE_NAME}')
-engine = create_engine(f'postgresql+psycopg2://root:root@127.0.0.1:5432/food')
+engine = create_engine(f'postgresql+psycopg2://{DATABABE_USER}:{DATABASE_PASSWORD}@pg_last:5432/food')
 Session = sessionmaker(bind=engine)
 Metadata = MetaData()
 Metadata.reflect(bind=engine)
